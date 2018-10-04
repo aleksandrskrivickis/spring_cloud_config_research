50e43127a792d716533ec19a3b125acf08510438
ak7993g <ak7993g@gre.ac.uk>
2018-10-04 12:22:04 +0800

    Cloud config client now fetches config from both MySQL and GitHub.

--- implementation/combined-server/servermysqlgit/src/main/java/configsrv/main.java
+++ implementation/combined-server/servermysqlgit/src/main/java/configsrv/main.java
@@ -3,11 +3,23 @@ package configsrv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.environment.JGitEnvironmentRepository;
+import org.springframework.cloud.config.server.environment.JdbcEnvironmentRepository;
+import org.springframework.context.annotation.Import;
+
 
 @EnableConfigServer
 @SpringBootApplication
+@Import({
+        JdbcEnvironmentRepository.class,
+        JGitEnvironmentRepository.class
+})
+
+
 
 public class main {
+
+
     public static void main(String[] args) {
         SpringApplication.run(main.class, args);
     }
